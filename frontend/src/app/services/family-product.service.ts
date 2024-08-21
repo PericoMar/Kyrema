8dd4db0c46589134c068be08ba68cdd37b094640
Pedro Martinez Gonzalez <pericomarytb@gmail.com>
2024-08-21 16:38:01 +0200

    chore: Update API_URL in family-product.service.ts

--- frontend/src/app/services/family-product.service.ts
+++ frontend/src/app/services/family-product.service.ts
@@ -12,23 +12,23 @@ export class FamilyProductService {
   constructor(private http : HttpClient) { }
 
   getTiposProductoPorSociedad(id_sociedad: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}tipos-producto/sociedad/${id_sociedad}`);
+    return this.http.get<any>(`${this.apiUrl}/tipos-producto/sociedad/${id_sociedad}`);
   }
 
   getTipoProductoPorId(id_tipo_producto: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}tipo-producto/show/${id_tipo_producto}`);
+    return this.http.get<any>(`${this.apiUrl}/tipo-producto/show/${id_tipo_producto}`);
   }
 
   getTipoProductoPorLetras(letras_identificacion: string): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}tipo-producto/${letras_identificacion}`);
+    return this.http.get<any>(`${this.apiUrl}/tipo-producto/${letras_identificacion}`);
   }
 
   getAllTipos(): Observable<any> {
-    return this.http.get<any>(`${this.apiUrl}tipos-producto/all`);
+    return this.http.get<any>(`${this.apiUrl}/tipos-producto/all`);
   }
 
   deleteTipoProducto(id_tipo_producto: string): Observable<any> {
-    return this.http.delete<any>(`${this.apiUrl}tipo-producto/delete/${id_tipo_producto}`);
+    return this.http.delete<any>(`${this.apiUrl}/tipo-producto/delete/${id_tipo_producto}`);
   }
   
 }
