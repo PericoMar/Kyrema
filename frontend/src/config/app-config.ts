c7cb69dbae4fa3b4ea792ed70ec46f8472e89bd5
PericoMar <pericomarytb@gmail.com>
2024-09-09 11:29:50 +0200

    Update app-config.ts changed API_URL

--- frontend/src/config/app-config.ts
+++ frontend/src/config/app-config.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-    // API_URL: 'https://canama-backend-afcegzdrh8brgvc8.spaincentral-01.azurewebsites.net/api',
-    API_URL: 'http://localhost:8000/api',
+    API_URL: 'https://canama-backend-afcegzdrh8brgvc8.spaincentral-01.azurewebsites.net/api',
+    // API_URL: 'http://localhost:8000/api',
     APP_NAME: 'My Angular App',
     SOCIEDAD_ADMIN_ID: '1',
     PREFIJO_LETRAS_IDENTIFICACION_ANEXOS: 'ANEXOS_',
